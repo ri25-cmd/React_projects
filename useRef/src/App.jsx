110c7de3ce494a2050aa80625a885361e4bcfd89
ri25-cmd <riddhitiwarir38@gmail.com>
2025-10-12 10:59:34 +0530

    Add new project demonstrating useRef hook

--- useRef/src/App.jsx
+++ useRef/src/App.jsx
@@ -1,33 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState } from "react"
+
+//useRef
+//refrence to a value, such that when you change the value, the component doesn't re-render
 
 function App() {
-  const [count, setCount] = useState(0)
+  const[currTime, setcurrTime]= useState(0);
+  const inputref= useRef();
+  const timer = useRef();
+
+  //we assign value to a variable in 3 ways:::
+  //1) let num = 1 => this is the rearest way in which we assign the values
+  //we use either of th e below 2 to assign the value
+ // const [value, setValue] = useState(1);
+  //or
+ //  const valRef = useRef()
+
+  const Applyref = () =>{
+    inputref.current.focus();
+  }
+
+  const Startclock = () =>{
+ 
+    const value = setInterval(() => {
+      setcurrTime(c => c+1);
+    }, 1000)
+    timer.current = value;
+  }
+
+  const Stopclock =() =>{
+  
+    clearInterval(timer.current)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <input ref={inputref} type="text"></input>
+      <button onClick={Applyref}>Add text</button>
+      <div style={{padding:10, margin:10}}>
+        {currTime}
+        <br></br>
+        <button onClick={Startclock}>Start</button>
+        <button onClick={Stopclock}>Stop</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
